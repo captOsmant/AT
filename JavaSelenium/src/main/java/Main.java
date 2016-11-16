94d93b88aa6a0796ffad8b876f859be208bdb547
Denis Latushkin <retarcorp@gmail.com>
2016-11-16 17:07:22 +0200

    Added selenium tests

---
+++ JavaSelenium/src/main/java/Main.java
@@ -0,0 +1,40 @@
+/**
+ * Created by Captain Osmant on 16.11.2016.
+ */
+import com.gargoylesoftware.htmlunit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
+
+//import org.seleniumhq.selenium;
+import org.openqa.*;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Launched JAVA project");
+
+        WebDriver driver = new InternetExplorerDriver();
+//        String path = "http://at.retarcorp.com/core";
+//        driver.get(path);
+//        System.out.println(driver.getTitle());
+//
+//        WebElement element = driver.findElement(By.name("login"));
+//
+//        element.sendKeys("retarcorp");
+//
+//        WebElement form = driver.findElement(By.tagName("form"));
+//        form.submit();
+
+
+
+
+
+        return;
+
+
+    }
+}
